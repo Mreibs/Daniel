e3dea7d6912f121b6b7680f47e302ccb96d66417
Mreibs <ebbesen_2@hotmail.com>
2025-03-27 01:20:23 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-lysminister/
-├── package.json ✅
-├── vite.config.js ✅
-├── index.html ✅
-├── /src
-    └── LysSim.jsx ✅
-    └── main.jsx ✅
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+});
